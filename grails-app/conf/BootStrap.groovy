d9a0810de7f029a7dac631712c6a3a769d6060af
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2015-02-07 11:09:44 +0100

    minor

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -4,9 +4,16 @@ import bootstrap.CommerceService
 import bootstrap.PerfCommerceService
 
 class BootStrap {
+    // Nombre de catégories principales
     public static final int LEVEL_ONE_CATEGORY = 10
+
+    // Nombre de sous catégories par catégorie principale
     public static final int LEVEL_TWO_CATEGORY = 5
+
+    // Nombre de produits par sous catégorie
     public static final int MAX_PRODUCTS_PER_CATEGORY = 10
+    // le nombre total de produit est le produit des trois valeurs ci-dessus (soit dans ce cas 5 * 10 * 10 = 500)
+
     def grailsApplication
     CommonService commonService
     CommerceService commerceService
